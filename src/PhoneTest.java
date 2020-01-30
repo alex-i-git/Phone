213a8a9492fc4ef76abc394ebe19c7658ee60807
alex-i-git <a.izmaylove@gmail.com>
2020-01-30 17:05:45 +0300

    *

---
+++ src/PhoneTest.java
@@ -0,0 +1,16 @@
+public class PhoneTest {
+    public static void main(String[] args) {
+
+
+        Phone ph1 = new Phone(1,2,3);
+        Phone ph2 = new Phone(4,5,6);
+        Phone ph3 = new Phone(7,8,9);
+
+
+
+         System.out.println(ph1.number + " " + ph1.model + " " + ph1.weight);
+        System.out.println(ph2.number + " " + ph2.model + " " + ph2.weight);
+        System.out.println(ph3.number + " " + ph3.model + " " + ph3.weight);
+
+    }
+}
